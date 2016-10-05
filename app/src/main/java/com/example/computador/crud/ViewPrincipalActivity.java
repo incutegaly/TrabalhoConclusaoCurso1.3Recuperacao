84c7236424fe1f346bbdfb0b2de3ac7b5dea48ae
COMPUTADOR <matheusimonpaes@gmail.com>
2016-10-05 04:54:36 -0300

    05/10

--- app/src/main/java/com/example/computador/crud/ViewPrincipalActivity.java
+++ app/src/main/java/com/example/computador/crud/ViewPrincipalActivity.java
@@ -1,12 +1,18 @@
 package com.example.computador.crud;
 
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.FragmentManager;
+
 import android.support.v7.app.AlertDialog;
+
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,14 +22,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+
 
 public class ViewPrincipalActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     private static final String PREF_NAME = "LoginActivityPreference";
     private AlertDialog alerta; //atributo da classe
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +54,8 @@ public class ViewPrincipalActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -87,18 +94,20 @@ public class ViewPrincipalActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        FragmentManager fragmentManager = getSupportFragmentManager();
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
+        } else if (id == R.id.nav_maps) {
+            Intent intent = new Intent (this, MapsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_usuario) {
-            Intent intent = new Intent (this, UsuarioAlterarDados.class);
+            Intent intent = new Intent(this, UsuarioAlterarDados.class);
             startActivity(intent);
         } else if (id == R.id.nav_logout) {
             logout();
@@ -138,4 +147,7 @@ public class ViewPrincipalActivity extends AppCompatActivity
         //Exibe
         alerta.show();
     }
+
+
+
 }
